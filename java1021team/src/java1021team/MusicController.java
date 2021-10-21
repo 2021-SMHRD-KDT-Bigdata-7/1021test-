23b5018402b6615bc338221b8a6a1976c58b8e47
smhrd <smhrd@DESKTOP-JR21QM5>
2021-10-21 12:50:51 +0900

    컨트롤러

--- java1021team/src/java1021team/MusicController.java
+++ java1021team/src/java1021team/MusicController.java
@@ -1,5 +1,46 @@
 package java1021team;
 
+import java.util.ArrayList;
+
 public class MusicController {
+
+	ArrayList<MusicVO> musiclist = new ArrayList<>();
+
+	int index = 0;
+
+	
+	
+	          
+	    //노래 정보를 출력할수 있는 메소드
+	public void print(MusicVO m) {
+	         System.out.println(m.getSongName() + ", " + m.getSinger() + ", "
+	               +m.getPlayTime());
+
+	// 실행 메소드
+	public MusicVO play() {
+		return musiclist.get(index);
+	}
+
+	// 이전곡 재생
+	public MusicVO pre() {
+		if (index > 0) {
+			index--;
+		} else {
+			index = musiclist.size() - 1;
+
+		}
+		return musiclist.get(index);
+
+	}
+
+	// 다음곡 재생
+	public MusicVO next() {
+		if (index < musiclist.size() - 1) {
+			index++;
+		} else {
+			index = 0;
+		}
+		return musiclist.get(index);
+	}
 
 }
