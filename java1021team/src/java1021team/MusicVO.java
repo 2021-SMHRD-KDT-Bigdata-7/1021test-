3e0068f82a3a3df7cd9e32a486703260e055a9dd
smhrd <smhrd@DESKTOP-NQPB5SO>
2021-10-21 12:15:43 +0900

    MusicVO

--- java1021team/src/java1021team/MusicVO.java
+++ java1021team/src/java1021team/MusicVO.java
@@ -1,5 +1,47 @@
 package java1021team;
 
 public class MusicVO {
+	
+	String title;
+	String singer;
+	int playTime;
+	
+	public MusicVO () {}
+	
+	public MusicVO(String title, String singer, int playTime) {
+		super();
+		this.title = title;
+		this.singer = singer;
+		this.playTime = playTime;
+	
+}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSinger() {
+		return singer;
+	}
+
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+
+	public int getPlayTime() {
+		return playTime;
+	}
+
+	public void setPlayTime(int playTime) {
+		this.playTime = playTime;
+	}
+
+	@Override
+	public String toString() {
+		return "MusicVO [title=" + title + ", singer=" + singer + ", playTime=" + playTime + "]";
+	}
 }
